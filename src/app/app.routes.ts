8a73ccef57e96e6f4aa9b3bfb5655f850cc461c8
Khanomtom <146657653+Khanomtom@users.noreply.github.com>
2024-02-28 21:56:31 +0700

    joo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { VotenologinComponent } from './components/votenologin/votenologin.component';
+import { StandingComponent } from './components/standing/standing.component';
 
 
 export const routes: Routes = [
     {path:'',component:IndexComponent},
     {path:'regis',component:RegisterComponent},
     {path:'login',component:LoginComponent},
+    {path:'guess',component:VotenologinComponent},
+    {path:'stand',component:StandingComponent}
 ];
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
+  constructor(private router:Router){
+
+  }
+test() {
+  this.router.navigate(['guess']);
+}
 
 }
