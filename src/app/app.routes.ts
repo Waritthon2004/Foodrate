f21f9313caead70e064de9af4f967cf9a75342c5
Khanomtom <146657653+Khanomtom@users.noreply.github.com>
2024-02-28 20:01:28 +0700

    index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'',component:IndexComponent},
+    {path:'regis',component:RegisterComponent},
+    {path:'login',component:LoginComponent},
+];
