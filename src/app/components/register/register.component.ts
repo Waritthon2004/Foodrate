a1a59e2194609ff3e17db013fb2bbab42b2cc7f1
Khanomtom <146657653+Khanomtom@users.noreply.github.com>
2024-03-01 20:09:13 +0700

    update

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder,FormGroup } from '@angular/forms';
+import { ApiService } from '../../service/api.service';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  myform! : FormGroup;
+  constructor(private formBuilder : FormBuilder,private api : ApiService){}
+  ngOnInit(): void {
+    this.myform = this.formBuilder.group({
+      FirstName: [''],
+      LastName:[''],
+      Email:[''],
+      Password: [''],
+    });
+  }
+ 
+  async submitForm() {
+    const formData = this.myform.value;
+    console.log(formData);
+    
+    const response =  await this.api.register(formData);
+    //console.log(response);
+    
+  }
 
 }
