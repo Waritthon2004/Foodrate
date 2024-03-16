84d5085ce0e333b7c1dca1ed95757d0fbf0c5dbd
Khanomtom <146657653+Khanomtom@users.noreply.github.com>
2024-03-17 04:35:07 +0700

    pop

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service';
+import { CommonModule } from '@angular/common';
 import {
   MatDialog,
   MatDialogRef,
@@ -15,19 +16,24 @@ import {
     MatDialogActions,
     MatDialogClose,
     MatDialogTitle,
-    MatDialogContent,],
+    MatDialogContent,CommonModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
 export class DialogComponent implements OnInit {
   data! : any;
+  winer: any;
   constructor(private api: ApiService){
     
   }
   ngOnInit(): void {
    this.data = this.api.cal;
    console.log(this.data);
-   
   }
+  // check(){
+  //   this.winer = this.data.Win
+  //   console.log("The Winer is "+this.winer);
+    
+  // }
   
 }
